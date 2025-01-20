4360ae000a8d700cface47c328adf4b2811584be
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2025-01-20 18:31:46 +0000

    Add: create Food.class

---
+++ src/main/java/model/Food.java
@@ -0,0 +1,14 @@
+package model;
+
+public record Food(String name, Double calories, Double Protein , Double fat, Double carbohydrates) {
+    @Override
+    public String toString() {
+        return "model.Food{" +
+                "name='" + name + '\'' +
+                ", calories=" + calories + "kcal" +
+                ", Protein=" + Protein + "g" +
+                ", fat=" + fat + "g" +
+                ", carbohydrates=" + carbohydrates + "g" +
+                '}';
+    }
+}
